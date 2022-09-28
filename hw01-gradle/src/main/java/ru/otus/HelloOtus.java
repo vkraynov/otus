3de23568f782baf19e991ca3685f8ec352bf9de2
vkraynov <electropo4ta2015@yandex.ru>
2022-09-29 00:50:58 +0300

    Made gradle project with module structure.

---
+++ hw01-gradle/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,12 @@
+package ru.otus;
+
+import com.google.common.math.BigIntegerMath;
+
+/**
+ * Main class.
+ */
+public class HelloOtus {
+    public static void main(String... args) {
+        System.out.println(BigIntegerMath.factorial(100));
+    }
+}
